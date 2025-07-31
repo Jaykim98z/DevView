d572bd9c4a587ea69966b854400716aaaba14be9
sswck <sswcg@daum.net>
2025-07-31 15:16:46 +0900

    Feat: Add JpaRepositories for Interview domain

--- src/main/java/com/allinone/DevView/interview/repository/InterviewRepository.java
+++ src/main/java/com/allinone/DevView/interview/repository/InterviewRepository.java
@@ -1,4 +1,7 @@
 package com.allinone.DevView.interview.repository;
 
-public class InterviewRepository {
+import com.allinone.DevView.interview.entity.Interview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
 }
